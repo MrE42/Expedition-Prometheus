e268fd306d83234198dc9faef703534409eba2e7
Elijah Schoneweis <89532300+MrE42@users.noreply.github.com>
2023-04-24 16:35:50 -0500

    Bullet Particles?

--- Assets/BulletDamage.cs
+++ Assets/BulletDamage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,23 @@ using UnityEngine;
 public class BulletDamage : MonoBehaviour
 {
     public int damage = 1;
+    private GameObject shotType;
+    public GameObject regularShot;
+    public GameObject chargedShot;
+    public void Generate(int d)
+    {
+        damage = d;
+        if (damage == 1)
+        {
+            shotType = regularShot;
+        }
+        else
+        {
+            shotType = chargedShot;
+        }
+        for (int i = 0; i < shotType.transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.GetComponent<ParticleSystem>().Play();
+        }
+    }
 }
