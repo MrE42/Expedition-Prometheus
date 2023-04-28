362c10f84fa4d17b9e618d13a68033bde0fe9f5b
Gabriel Clark <gabrielclark3330@gmail.com>
2023-04-28 15:55:33 -0500

    Made glowing materials emissive

--- Assets/FuseColorUpdate.cs
+++ Assets/FuseColorUpdate.cs
@@ -15,5 +15,7 @@ public class FuseColorUpdate : MonoBehaviour
     void Update()
     {
         gameObject.GetComponent<MeshRenderer>().material.color = new Color(0, gameObject.transform.parent.gameObject.GetComponent<FusePower>().charge, 0);
+        gameObject.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+        gameObject.GetComponent<Renderer>().material.SetColor("_EmissionColor", new Vector4(0, gameObject.transform.parent.gameObject.GetComponent<FusePower>().charge, 0, 0) * 5);
     }
 }
