8579ab83fd9ba58007723da5bf19eebe004a389a
Gabriel Clark <gabrielclark3330@gmail.com>
2023-04-30 21:39:27 -0500

    Added different enemy types

--- Assets/LevelControl.cs
+++ Assets/LevelControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LevelControl : MonoBehaviour
 {
-    public List<List<int>> waves = new List<List<int>> { new List<int> {1}, new List<int> {1}, new List<int> {1,1}, new List<int> {1,1}, new List<int> {1}, new List<int> {1,1}, new List<int> {1,1,1}, new List<int> {1,1,1,1}, new List<int> {1,1,1}, new List<int> {1}}; // each index is an array were [num of enemy 1, num of enemy 2, num of enemy 3]
+    public List<List<int>> waves = new List<List<int>> { new List<int> {1}, new List<int> {2}, new List<int> {1,2}, new List<int> {2,2}, new List<int> {3}, new List<int> {1,3}, new List<int> {1,2,3}, new List<int> {1,1,3,3}, new List<int> {1,1,1}, new List<int> {1}}; // each index is an array were [num of enemy 1, num of enemy 2, num of enemy 3]
     public float spawnRate = 15;
     public Spawner spawnerMain;
     public Spawner spawnerRight;
